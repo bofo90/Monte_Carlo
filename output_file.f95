29c68beb1cb95095ca712444d8b4e8d48b864a58
epolimpio <epolimpio@gmail.com>
2015-03-19 16:35:14 +0100

    Output to file

---
+++ output_file.f95
@@ -0,0 +1,48 @@
+module output_file
+    
+    use global
+    
+    implicit none
+
+    integer, parameter :: fRsqr = 1001
+
+contains
+
+    subroutine init_files(fname)
+
+        character(len=*), intent(in) :: fname
+
+        open(unit = fRsqr, file = "./data/energy_" // fname // ".dat")
+
+        call insert_header(fRsqr)
+
+    end subroutine init_files
+
+    subroutine insert_header(file)
+
+        integer, intent(in) :: file
+
+        write(file, *) "BETA->", beta
+
+    end subroutine
+
+    subroutine write_rsqr
+
+        integer(8) :: i
+        real(8) :: mean_rsqr
+
+        do i = 1, N
+          mean_rsqr = rsqr(i)/sum_weight(i)
+          write(fRsqr, *) i, mean_rsqr, sqrt(rsqr(i)/sum_weight(i) - mean_rsqr**2)
+        end do
+
+    end subroutine write_rsqr
+
+    subroutine close_files
+
+        close(fRsqr)
+
+    end subroutine close_files
+
+
+end module
