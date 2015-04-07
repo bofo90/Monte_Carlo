dec4febe6d42af80a0ed50c12ff87b6d4406f3b2
Agustin <f.agustin@gmail.com>
2015-04-07 14:04:31 +0200

    nonthing done

--- chain_grow.f95
+++ chain_grow.f95
@@ -26,14 +26,14 @@ contains
     pol_weight = 1._8
     L = 3
     if (perm .AND. i > i_PERM) then
-      call add_bead(pos, pol_weight, L, .TRUE.)
+       call add_bead(pos, pol_weight, L, .TRUE.)
     else
-      call add_bead(pos, pol_weight, L, .FALSE.)
+       call add_bead(pos, pol_weight, L, .FALSE.)
     end if
 
     ! Check the positions of the first polymers
     if (i<7 .AND. .NOT. perm) then
-      call write_pos(pos)
+       call write_pos(pos)
     end if
   end subroutine chain_grow
 
@@ -56,38 +56,38 @@ contains
     call r_statistics(pos_now, position, pol_weight)
     call up_and_low_limit_calc(pos_now, up_limit, low_limit)
     if (perm) then
-      if (pos_now < N .and. pol_weight > 0) then
-        if (pol_weight > up_limit) then
-          pol_weight = pol_weight * 0.5
-          print *, pos_now, "ENRICH", up_limit, low_limit, pol_weight
-          call add_bead(position, pol_weight, pos_now+1, perm)
-          call add_bead(position, pol_weight, pos_now+1, perm)
-        else if(pol_weight < low_limit) then
-          call init_random_seed
-          call random_number(random)
-          if(random < 0.5_8) then
-             pol_weight = pol_weight * 2
-             print *, pos_now, "KEEP", up_limit, low_limit, pol_weight
+       if (pos_now < N .and. pol_weight > 0) then
+          if (pol_weight > up_limit) then
+             pol_weight = pol_weight * 0.5
+             print *, pos_now, "ENRICH", up_limit, low_limit, pol_weight
              call add_bead(position, pol_weight, pos_now+1, perm)
+             call add_bead(position, pol_weight, pos_now+1, perm)
+          else if(pol_weight < low_limit) then
+             call init_random_seed
+             call random_number(random)
+             if(random < 0.5_8) then
+                pol_weight = pol_weight * 2
+                print *, pos_now, "KEEP", up_limit, low_limit, pol_weight
+                call add_bead(position, pol_weight, pos_now+1, perm)
+             else
+                print *, pos_now, "PRUNE", up_limit, low_limit, pol_weight
+             end if
           else
-            print *, pos_now, "PRUNE", up_limit, low_limit, pol_weight
+             call add_bead(position, pol_weight, pos_now+1, perm)
           end if
-        else
-          call add_bead(position, pol_weight, pos_now+1, perm)
-        end if
-      else
-        if(pos_now == N .AND. num_N_poly(pos_now) < 6) then
-          call write_pos(position)
-        else
-          print *, pos_now, "KILL", pol_weight
-        end if
-      end if
+       else
+          if(pos_now == N .AND. num_N_poly(pos_now) < 6) then
+             call write_pos(position)
+          else
+             print *, pos_now, "KILL", pol_weight
+          end if
+       end if
     else
-      if (pol_weight > 0._8 .AND. pos_now < N) then
-        call add_bead(position, pol_weight, pos_now+1, perm)
-      end if
+       if (pol_weight > 0._8 .AND. pos_now < N) then
+          call add_bead(position, pol_weight, pos_now+1, perm)
+       end if
     end if
 
   end subroutine add_bead
 
-end module
+end module growing
